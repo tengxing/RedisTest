c98828c2af632b55132b8cbab1d44ea689bf29fb
tengxing <tengxing7452@163.com>
2017-08-21 23:45:00 +0800

    配置

--- doc/redis-sentinel/bin/conn-cli.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "连接redis端口：$1"
-sudo /usr/local/bin/redis-cli -c -h 127.0.0.1 -p $1 
-
--- doc/redis-sentinel/bin/shutdown.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo /usr/local/bin/redis-cli -h 127.0.0.1 -p 8001 shutdown
-sudo /usr/local/bin/redis-cli -h 127.0.0.1 -p 8101 shutdown
-sudo /usr/local/bin/redis-cli -h 127.0.0.1 -p 8201 shutdown
-sudo /usr/local/bin/redis-cli -h 127.0.0.1 -p 8301 shutdown
-sudo /usr/local/bin/redis-cli -h 127.0.0.1 -p 8015 shutdown
-sudo /usr/local/bin/redis-cli -h 127.0.0.1 -p 8006 shutdown
